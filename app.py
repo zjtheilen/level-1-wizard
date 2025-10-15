d30b526b4f8b8314901b53951a192e812a4ae6a3
zjtheilen <zjtheilen@gmail.com>
2025-10-15 11:38:34 -0700

    remove print statements, error if any fields are submitted blank

--- app.py
+++ app.py
@@ -13,26 +13,22 @@ def home():
         player_name = request.form.get("player_name")
         character_name = request.form.get("character_name")
 
-        if not race_name or not class_name:
-            error_message = "Need both race and class before moving on."
+        if not race_name or not class_name or not player_name or not character_name:
+            error_message = "Must complete all fields before moving on."
             return render_template("index.html",
                                    races=races,
                                    classes=classes,
                                    character=None,
                                    error=error_message)
 
-        print(f"User selected race={race_name}, class={class_name}")
-
         race = CharacterRace(race_name)
         char_class = CharacterClass(class_name)
+
         character = Character(
             player_name=player_name, 
             character_name=character_name,
             race=race,
             char_class=char_class)
-
-        print(f"{character.player_name.capitalize()} is playing a "
-              f"{character.race} {character.char_class} named {character.character_name.capitalize()}.")
 
         return render_template("index.html",
                                races=races,
