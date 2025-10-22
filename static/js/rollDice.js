9fbd8abe9f51b2616514683564b6be711c898072
zjtheilen <zjtheilen@gmail.com>
2025-10-22 09:44:44 -0700

    separate rolling / assigning functions

--- static/js/rollDice.js
+++ static/js/rollDice.js
@@ -1,26 +1,29 @@
 function rollAbilities() {
     const rolls = [];
+
     for (let i = 0; i < 7; i++) {
-        let results = [];
-        let topThreeSum = 0;
+        let topThreeSum;
         do {
-            results = [];
-            for (let i = 0; i < 4; i++) {
-                results.push(Math.floor(Math.random() * 6) + 1);
-            }
-            const sorted = [...results].sort((a, b) => b - a);
+            const dice = Array.from({ length: 4 }, () => Math.floor(Math.random() * 6) + 1);
+            const sorted = dice.sort((a, b) => b - a);
             topThreeSum = sorted.slice(0, 3).reduce((a, b) => a + b, 0);
         } while (topThreeSum < 8);
+
         rolls.push(topThreeSum)
     }
     const smallestIndex = rolls.indexOf(Math.min(...rolls));
     if (smallestIndex !== -1) {
         rolls.splice(smallestIndex, 1);
     }
-    document.getElementById("roleScores").innerText = rolls
+    document.getElementById("roleScores").innerText = rolls.join(", ")
 
+    assignAbilities(rolls)
+}
+
+function assignAbilities(rolls) {
     const abilitySelects = document.querySelectorAll(".stat-assigner select");
     abilitySelects.forEach(select => {
+        select.innerHTML = '<option value="">Select value</option>';
         rolls.forEach((r, idx)=> {
             const opt = document.createElement("option");
             opt.value = r;
@@ -28,5 +31,6 @@ function rollAbilities() {
             select.appendChild(opt)
         });
     });
+    
     window.rolledAbilities = rolls;
 }
